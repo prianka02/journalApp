737cfe17f93bea74cf5b6e7b258c054d1a443e4a
Prianka <priankaakter115@gmail.com>
2025-02-25 00:35:14 +0600

    Initial commit

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/HealthCheck.java
@@ -0,0 +1,15 @@
+package net.engineeringdigest.journalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "OK";
+    }
+}
+
+// It checks where our spring boot app run correctly or not! Just use for initial test cases
